b12327fca44c1607a5b3c6ba1a413534d9d0c17f
jodyyan <jodyyan0726@gmail.com>
2023-11-23 20:56:04 +0800

    create class project controllers and models

---
+++ app/Models/Consultant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Consultant extends Model
+{
+    protected $table = 'consultants';
+
+    use HasFactory;
+}
---
+++ database/factories/ConsultantFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ConsultantFactory extends Factory
+{
+    protected static ?string $password;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'sex' => 'male',
+            'nationality' => 'USA',
+            'introduction' => fake()->sentence(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => static::$password ??= Hash::make('password'),
+            'enabled' => 1,
+
+        ];
+    }
+}
